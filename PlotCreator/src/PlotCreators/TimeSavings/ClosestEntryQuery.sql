9fd3795424bb7b38bf95fe92568dd4304f8188a9
Berberer <lukas.brandt@veds.de>
2019-03-25 12:49:07 +0100

    more results will be used for the average to smooth out results

--- PlotCreator/src/PlotCreators/TimeSavings/ClosestEntryQuery.sql
+++ PlotCreator/src/PlotCreators/TimeSavings/ClosestEntryQuery.sql
@@ -1,4 +1,7 @@
-SELECT achievedSampleSize, samplingTime, trainingTime FROM subsampling_results_with_sample_sizes
-WHERE dataset='DATASET' AND model='MODEL' AND algorithm = 'ALGORITHM' AND score IS NOT NULL
-ORDER BY ABS( achievedSampleSize - VALUE )
-LIMIT 1
+SELECT AVG(results.achievedSampleSize) AS achievedSampleSize, AVG(results.samplingTime) AS samplingTime, AVG(results.trainingTime) AS trainingTime FROM
+  (
+    SELECT * FROM subsampling_results_with_sample_sizes
+    WHERE dataset='DATASET' AND model='MODEL' AND algorithm = 'ALGORITHM' AND score IS NOT NULL
+    ORDER BY ABS( achievedSampleSize - VALUE )
+    LIMIT 5
+  ) results
